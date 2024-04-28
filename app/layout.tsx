56d570567a8805432bd71fef3be3b3e9340798ad
lz1262 <167993946+lz1262@users.noreply.github.com>
2024-04-28 20:41:15 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,16 @@ const LocaleLayout = ({
   const locale = getLocaleOnServer()
   return (
     <html lang={locale ?? 'en'} className="h-full">
-      <body className="h-full">
+       <body className="h-full">
+      {/* 导航栏 */}
+       <nav className="flex justify-between items-center bg-gray-200 p-4">
+          <div>
+            <a href="/">首页</a>
+            <a href="/feature1" className="ml-4">功能1</a>
+            <a href="/feature2" className="ml-4">功能2</a>
+            <a href="/feature3" className="ml-4">功能3</a>
+          </div>
+        </nav>
         <div className="overflow-x-auto">
           <div className="w-screen h-screen min-w-[300px]">
             {children}
